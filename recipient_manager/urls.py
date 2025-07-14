4c6873f6e6428c4eb94530de09c53bc7cf530f13
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2025-07-14 18:15:21 +0300

    .

--- recipient_manager/urls.py
+++ recipient_manager/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path, include
+from django.urls import path
+
 from recipient_manager.apps import RecipientManagerConfig
 from recipient_manager.views import (MailingRecipientListView, HomeView, MailingRecipientDetailView,
                                      MailingRecipientCreateView, MailingRecipientDeleteView, MailingRecipientUpdateView)
-
 
 app_name = RecipientManagerConfig.name
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('recipients/', MailingRecipientListView.as_view(), name='recipient_list'),
-    path('recipients/create/', MailingRecipientCreateView.as_view(), name='recipient_create'),
-    path('recipients/<int:pk>/', MailingRecipientDetailView.as_view(), name='recipient_detail'),
-    path('recipients/<int:pk>/delete/', MailingRecipientDeleteView.as_view(), name='recipient_delete'),
-    path('recipients/<int:pk>/update/', MailingRecipientUpdateView.as_view(), name='recipient_update'),
+    path('', MailingRecipientListView.as_view(), name='recipient_list'),
+    path('', MailingRecipientCreateView.as_view(), name='recipient_create'),
+    path('', MailingRecipientDetailView.as_view(), name='recipient_detail'),
+    path('', MailingRecipientDeleteView.as_view(), name='recipient_delete'),
+    path('', MailingRecipientUpdateView.as_view(), name='recipient_update'),
 ]
